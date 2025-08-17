a8cc91bf3e544a29348d547204ce22ce8ba1e0f7
Mohamed <muhamadsane114@gmail.com>
2025-08-17 17:43:17 +0300

    passed

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,27 @@
 class MySet:
+    def __init__(self, enumerable=[]):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+
+    def size(self):
+        return len(self.dictionary)
+
+    def clear(self):
+        self.dictionary.clear()
+        return self
+
+    def __str__(self):
+        set_list = [str(key) for key in self.dictionary.keys()]
+        return f'MySet: {{{", ".join(set_list)}}}'
